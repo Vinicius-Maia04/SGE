d0c91e0ee09d36f5c0e492aecb266192aa808956
Vinicius-Maia04 <vinimaia200@gmail.com>
2024-03-21 19:34:13 -0300

    SGE
    Projeto Sistema de Gestão de Ensino

---
+++ frontend/sge-front/nuxt.config.ts
@@ -0,0 +1,47 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  typescript: {
+    typeCheck: true
+  },
+  modules: [
+    'nuxt-primevue',
+    '@sidebase/nuxt-auth'
+  ],
+  primevue: {
+    components: {
+      include: ['Button']
+    }
+  },
+  css: [
+    'primeicons/primeicons.css', //css dos ícones do primevue
+    'primevue/resources/themes/aura-light-green/theme.css', // css tema dos componentes primevue
+    '~/assets/style/global-variables.scss',  //variáveis css global customizado para toda a aplicação
+    '~/assets/style/global-project.scss',  //css global customizado para toda a aplicação
+  ],
+  auth: {
+    baseURL: 'http://localhost:8000', // Endereço do Backend
+    provider: {
+      type: 'local', // Web Token
+      endpoints: {
+        signIn: { path: '/token/login', method: 'post' }, //rota do login
+        signOut: { path: '/token/logout', method: 'post' }, //rota do logout
+        signUp: { path: '', method: 'get' }, //rota de cadastro
+        getSession: { path: '/users', method: 'get' }, //rota autenticada para verificação se foi autenticado c/ sucesso
+        //refresh: { } //rota para renovar o token (somente no type refresh)
+      },
+      token: {
+        signInResponseTokenPointer: '/auth_token', //indica o local do token access na resposta do backend
+        //maxAgeInSeconds: 600 //validade do token access
+        type: 'Token'
+      },
+      // refreshToken: { 
+      //   signInResponseRefreshTokenPointer: '', //local do token refresh
+      //   maxAgeInSeconds: 600, //validade do token refresh
+      // },
+      pages: {
+        login: '/' //endereço (rota) da página de login
+      }
+    }
+  }
+})
---
+++ frontend/sge-front/pages/home/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+    definePageMeta({
+        middleware: 'auth' // Essa tela será privada
+    })
+</script>
+
+<template>
+    <div>
+        <h1>Olá Tela Home!</h1>
+    </div>
+</template>
